19f51967ae8d262307bc187399746318e20e6c40
EDENORTE\JuPadilla <jhoanpadi@gmail.com>
2022-03-28 14:37:04 -0400

    All the data

---
+++ js/typing.js
@@ -0,0 +1,9 @@
+/**/
+
+var typed = new Typed('.typing', {
+    strings: ["Web Designer", "Web Developer", "Data Analytics", "Consulting", "Mobile Developer"],
+    typeSpeed: 100,
+    BackSpeed: 50,
+    loop: true
+})
+
